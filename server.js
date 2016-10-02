dae12370a2f07483fceab67d816b84e26a5618e6
MeenakshiRamanathan <meenakshiramanathan4@gmail.com>
2016-10-02 12:55:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,14 +5,27 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var article = {
-    title:'Article||Meenakshi Ramanathan',
-    heading:'Article-One',
+
+var articles= {
+var articleOne :  {
+    title:'Article-One||Meenakshi Ramanathan', 
+heading: 'Article-One',
+date: 'October 20',
+content: ` </p> I have to develop a web app...And this would be the 1st page of it</p><p> I have to develop a web app...And this would be the 1st page of it</p><p> I have to develop a web app...And this would be the 1st page of it</p><p> I have to develop a web app...And this would be the 1st page of it</p>`
+    },
+var articleTwo={title:'Article2||Meenakshi Ramanathan',
+    heading:'Article-Two',
     date:'October 20',
-    content: ` </p> I have to develop a web app...And this would be the 1st page of it</p><p> I have to develop a web app...And this would be the 1st page of it</p><p> I have to develop a web app...And this would be the 1st page of it</p><p> I have to develop a web app...And this would be the 1st page of it</p>`
-    };
-
-
+    content: ` </p> I have to develop a web app...And this would be the 2nd page of it</p><p> I have to develop a web app...And this would </p>`},
+var articleThree={title:'Article3||Meenakshi Ramanathan',
+    heading:'Article-Three',
+    date:'October 20',
+    content: ` </p> I have to develop a web app...And this would be the 3rd page of it</p>`},
+var articleFour={title:'Article4||Meenakshi Ramanathan',
+    heading:'Article-Four',
+    date:'October 20',
+    content: ` </p> I have to develop a web app...And this would be the 4th page of it</p>`}
+};
 
 function createTemplate(data){
     var title=data.title;
