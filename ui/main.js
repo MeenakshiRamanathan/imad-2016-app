a24b8e050aab943df23055dec5198e85354b8f2e
MeenakshiRamanathan <meenakshiramanathan4@gmail.com>
2016-10-08 10:10:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -33,18 +33,18 @@
 
 //Submit name
 var nameInput=document.getElementById('name');
-var names= nameInput.value;
+var name= nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onClick=function(){
     //Make a reqest to the server and send the name
     //Capture a list of names and render it as a list
     
     var names=['name1','name2','name3'];
-    var list="";
+    var list= "";
     for(var i=0;i<names.length;i++){
-        list='<li>' + names[i]+'</li>';
+        list=='<li>' + names[i]+'</li>';
     }
-    var ul=document.getElementById('nameslist');
-    ul.innerHTML=nameslist;
+    var ul=document.getElementById('namelist');
+    ul.innerHTML=list;
     
 };
