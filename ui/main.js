bf6cf6501255b0ede7bc36bc3a91ce8a6fa0c429
MeenakshiRamanathan <meenakshiramanathan4@gmail.com>
2016-10-08 08:37:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,34 @@
-console.log('Loaded!');
+//counter code
+ 
+ var button=getElementById('counter');
+ var counter=0;
+ Button.onClick=function(){
+     
+     //Make a request to the counter endpoint
+     
+     //Create a request object
+     var request=new XMLHttpRequest();
+     
+     //Capture the respone and store it in a variable..
+     request.onreadystatechange=function(){
+         if(request.readyState===XMLHttpRequest.DONE){
+             //Take some action
+             if(request.status===200){
+                 var counter=request.responseText;
+                 var span=document.getElementById('count');
+                 span.innerHTML=counter.toString();
+             }
+         }
+         //Not yet done
+     };
+     
+     //Render the variable in the correct span..
+     
+     //Make a request to the counter endpoint
+     request.open('GET','http://meenakshiramanathan.imad.hasura-app.io/counter',true);
+     request.send(null);
+     
+     counter=counter+1;
+     var span=getElementById('count');
+     span.innerHTML=counter.toString();
+ };
