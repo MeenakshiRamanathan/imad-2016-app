f1f34c8efa9c9dbccdc19923bb04cdd868387fae
MeenakshiRamanathan <meenakshiramanathan4@gmail.com>
2016-10-31 23:26:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,14 @@
-//counter code
- 
- 
+console.log('Loaded!');
+
+var element=document.getElementById('main-text');
+element.innerHTML='New value';
+
+var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft-1;
+    img.style.marginLeft=marginLeft+'px';
+}
+img.onclick=function(){
+    var interval=setInterval(marginRight,50);
+} ;
