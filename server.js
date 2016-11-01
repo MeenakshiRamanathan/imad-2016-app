d396469f43362223ca5b082791815b38b1cae0c1
MeenakshiRamanathan <meenakshiramanathan4@gmail.com>
2016-11-01 09:34:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ function createTemplate(data){
     var content=data.content;
     
 
-}
+
     
     
 var htmlTemplate = `<html> 
@@ -85,7 +85,7 @@ app.get('/', function (req, res) {
 });
 
 
-});
+
 
 
 app.get('/:articleName',function(req,res){
