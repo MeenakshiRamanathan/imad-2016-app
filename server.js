61bdfd519cfa20a27f345cbcd40aebb3f808f1b2
MeenakshiRamanathan <meenakshiramanathan4@gmail.com>
2016-11-01 16:42:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ var htmlTemplate = `<html xmlns="http://www.w3.org/1999/xhtml">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>${title}</title>
 
-<meta name="viewport" ,  initial-scale=1"/>
+<meta name="viewport" content="width-device-width ,  initial-scale=1"/>
 <link href="/ui/style.css" rel="stylesheet"/>
 
 </head>
