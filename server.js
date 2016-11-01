d47be268a71dd4a49a1beb24b69c97e5811927c6
MeenakshiRamanathan <meenakshiramanathan4@gmail.com>
2016-11-01 20:07:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
 
+var config={
+    user:'meenakshiramanathan',
+    database:'meenakshiramanathan',
+    host:'http://db.imad.hasura-app.io/',
+    port:'5432',
+    password:process.env.DB.PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -150,6 +158,20 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool = new Pool(config);
+app.get('/test-db',function(req,res){
+   pool.query('SELECT * FROM test',function(err,result){
+       if(err){
+           res.status(500).send(err,toString());
+           
+       }
+       else{
+           res.send(JSON.string(fy(result)));
+       }
+       
+   }) ;
+    
+});
 
 
 
