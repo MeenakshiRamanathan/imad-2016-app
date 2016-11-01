ef18af225c7b523fdf27c527eb5d68ea61727c6c
MeenakshiRamanathan <meenakshiramanathan4@gmail.com>
2016-11-01 16:32:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ var htmlTemplate = `<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>${title}</title>
-<link rel="stylesheet" type="text/css" href="style.css"/>
+<link rel="stylesheet" type="text/css" href="ui/style.css"/>
 
 </head>
 <body>
