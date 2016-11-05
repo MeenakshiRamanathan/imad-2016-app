7969ba39ff799b8017fa387bffb8018620993534
MeenakshiRamanathan <meenakshiramanathan4@gmail.com>
2016-11-05 19:11:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -182,6 +182,9 @@ comments.push(comment);
 
 //to render those comments on the page
 });
+app.get('/login', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'login.html'));
+});
 
 
 
