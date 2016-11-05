8ee691f6108e06524c9f7c03c646ed7e344d027b
MeenakshiRamanathan <meenakshiramanathan4@gmail.com>
2016-11-05 13:53:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -132,6 +132,24 @@ var request = new XMLHttpRequest();
  
  
  
- 
+ $(document).ready(function() {
+  $('li').on('click', function(){
+
+    var otherStars = $(this).parent().children();
+    otherStars.children('span').css('background-position', '0px 0px');
+    var onStarLength = $(this).data('length');
+
+    for(var i = 0; i < onStarLength; i++) {
+      $(otherStars[i]).children('span').css('background-position', '22px 22px');
+    }
+
+    if (onStarLength <= 2) {
+      $('p').fadeIn('slow').text("We apologise that you don't feel our service was upto your standards...");
+    }
+    else if (onStarLength >= 3) {
+      $('p').fadeIn('slow').text("Thanks for rating our service so highly!");
+    }
+  });
+});
  
  
