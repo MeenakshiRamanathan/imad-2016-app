5b5600223601694fc0ea8c5575e8075a2633f3d6
MeenakshiRamanathan <meenakshiramanathan4@gmail.com>
2016-11-11 16:09:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,29 +1,29 @@
-// counter code
+// // counter code
 
-var button=document.getElementById("counter");
-if (button !== undefined) {
-button.onclick = function(){
-var request = new XMLHttpRequest();
-    //create a request
- request.onreadystatechange= function(){
- if (request.readyState === XMLHttpRequest.DONE)
-  {
-      // take some action
-    if (request.status===200)
-       { 
-           //alert("ok");
-        var counter=request.responseText;
-        var span=document.getElementById("count");
-        span.innerHTML=counter.toString();
-       }
-  }
-};
-   // make the request
-    request.open('GET', 'http://http://meenakshiramanathan.imad.hasura-app.io', true);
-     request.send(null);
-}; 
-}
-// Comments
+// var button=document.getElementById("counter");
+// if (button !== undefined) {
+// button.onclick = function(){
+// var request = new XMLHttpRequest();
+ //    //create a request
+ // request.onreadystatechange= function(){
+ // if (request.readyState === XMLHttpRequest.DONE)
+  // {
+     //  // take some action
+    // if (request.status===200)
+//        { 
+   //         //alert("ok");
+      //   var counter=request.responseText;
+        // var span=document.getElementById("count");
+        // span.innerHTML=counter.toString();
+       // }
+  // }
+// };
+   // // make the request
+    // request.open('GET', 'http://http://meenakshiramanathan.imad.hasura-app.io', true);
+     // request.send(null);
+// }; 
+// }
+// // Comments
  
 var submit=document.getElementById("comment-btn");
 if (submit !== undefined) {
