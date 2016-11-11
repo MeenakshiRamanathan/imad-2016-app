1403cc71ea86fa80f4af202ec7f202f09aa67cd6
MeenakshiRamanathan <meenakshiramanathan4@gmail.com>
2016-11-11 16:49:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,10 @@
-var jq = document.createElement("script");
-
-jq.addEventListener("load", proceed); // pass my hoisted function
-jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js";
-document.querySelector("head").appendChild(jq);
+(function(){
+  var newscript = document.createElement('script');
+     newscript.type = 'text/javascript';
+     newscript.async = true;
+     newscript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js';
+  (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(newscript);
+})();
  // counter code
 
  //var button=document.getElementById("counter");
