ce0d64f7559b8f64be9d1104dbfc9196ab459272
MeenakshiRamanathan <meenakshiramanathan4@gmail.com>
2016-11-11 16:51:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,4 @@
-(function(){
-  var newscript = document.createElement('script');
-     newscript.type = 'text/javascript';
-     newscript.async = true;
-     newscript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js';
-  (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(newscript);
-})();
+window.document.write("https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>");
  // counter code
 
  //var button=document.getElementById("counter");
